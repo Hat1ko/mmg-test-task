ab11a95321caf74a8ee8c37682f365cc3fac95f7
Hat1ko <pasha1apple@gmail.com>
2021-01-26 22:13:53 +0200

    INIT

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+declare const module: any
+
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }))
+
+  app.enableCors()
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('MixIt')
+    .setDescription('The Test API description')
+    .setVersion('1.0')
+    .addTag('mmg-test')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, swaggerOptions)
+  SwaggerModule.setup('docs', app, document)
+
+  await app.listen(3000)
+
+  if (module.hot) {
+    module.hot.accept()
+    module.hot.dispose(() => app.close())
+  }
+}
+
+bootstrap()
